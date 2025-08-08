890cf0597e051b3fbab65aa4918ad4aeceab06b9
chiniwarsp <chiniwarsp@gmail.com>
2025-08-08 15:36:42 +0800

    test

---
+++ Projects/DFT-Project/call.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int a, b;
+
+  std::cout << "Enter two numbers: ";
+  std::cin >> a >> b;
+
+  int sum = a + b;
+
+  std::cout << "The sum of a and b is: " << sum << std::endl;
+
+  return 0;
+}
